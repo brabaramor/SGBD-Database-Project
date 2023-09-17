ba4a10900299d2ac25bab6ca14993e2699e8c4bb
Barbara <128275198+brabaramor@users.noreply.github.com>
2023-09-17 11:03:19 -0300

    Second commit (all inserts done)

--- Script.sql
+++ Script.sql
@@ -90,6 +90,41 @@ create table manutencao (
   CONSTRAINT fk_manutencao_id_veiculo FOREIGN KEY (id_veiculo) REFERENCES veiculo (id_veiculo)
 );
 
+
+--Inserções na tabela id_cidade
+INSERT INTO id_cidade (cod_cidade, nome_cidade, uf)
+VALUES (01, 'Ferraz de Vasconcelos', 'SP');
+
+INSERT INTO id_cidade (cod_cidade, nome_cidade, uf)
+VALUES (02, 'Canoas', 'RS');
+
+INSERT INTO id_cidade (cod_cidade, nome_cidade, uf)
+VALUES (03, 'Porto Alegre', 'RS');
+
+INSERT INTO id_cidade (cod_cidade, nome_cidade, uf)
+VALUES (04, 'Niterói', 'RJ');
+
+INSERT INTO id_cidade (cod_cidade, nome_cidade, uf)
+VALUES (05, 'Salvador', 'BA');
+
+
+--Inserções na tabela cliente_endereco
+INSERT INTO cliente_endereco (cod_endereco, nome_end, no_casa, complemento, bairro, cod_cidade, cep)*
+VALUES (01, 'Rua Tóquio', 141, null, 'Praia do Flamengo', 05, 41601245);
+
+INSERT INTO cliente_endereco (cod_endereco, nome_end, no_casa, complemento, bairro, cod_cidade, cep)*
+VALUES (02, 'Avenida Ernani do Amaral Peixoto', 35, null, 'Centro', 04, 24004900);
+
+INSERT INTO cliente_endereco (cod_endereco, nome_end, no_casa, complemento, bairro, cod_cidade, cep)*
+VALUES (03, 'Rua Duque de Caxias', 1304, 'ap 1', 'Centro Histórico', 03, 90010280);
+
+INSERT INTO cliente_endereco (cod_endereco, nome_end, no_casa, complemento, bairro, cod_cidade, cep)*
+VALUES (03, 'Rua Ferreira Viana', 76, 'ap 2', 'São José', 02, 92425140);
+
+INSERT INTO cliente_endereco (cod_endereco, nome_end, no_casa, complemento, bairro, cod_cidade, cep)*
+VALUES (03, 'Rua Ricardo Samuel de Araujo', 123, 'casa 2', 'Jardim Figueiredo', 01, 08526030);
+
+
 --Inserções na tabela cliente
 INSERT INTO cliente (cliente_nome, cliente_sobrenome, cpf, nascimento, tipo_cnh, cod_endereco)
 VALUES ('Jihyo', 'Park', 14725836914, '1997-02-01', 'B', 03);
@@ -106,34 +141,64 @@ VALUES ('Sana', 'Minatozaki', 78945612378, '1996-12-29', 'B', 04);
 INSERT INTO cliente (cliente_nome, cliente_sobrenome, cpf, nascimento, tipo_cnh, cod_endereco)
 VALUES ('Nayeon', 'Lim', 15975385214, '1995-09-22', 'B', 02);
 
---Inserções na tabela cliente_endereco
-INSERT INTO cliente_endereco (cod_endereco, nome_end, no_casa, complemento, bairro, cod_cidade, cep)
-VALUES (01, 'Rua Tóquio', 141, null, 'Praia do Flamengo', 05, 41601245);
 
-INSERT INTO cliente_endereco (cod_endereco, nome_end, no_casa, complemento, bairro, cod_cidade, cep)
-VALUES (02, 'Avenida Ernani do Amaral Peixoto', 35, null, 'Centro', 04, 24004900);
+--inserir informações dos preços dos veículos por dia, semana e mês
+INSERT INTO preco_veiculo (id_preco, id_veiculo, preco_dia, preco_semana, preco_mes)
+VALUES (11, 01, 250, 1575, 6000);
 
-INSERT INTO cliente_endereco (cod_endereco, nome_end, no_casa, complemento, bairro, cod_cidade, cep)
-VALUES (03, 'Rua Duque de Caxias', 1304, 'ap 1', 'Centro Histórico', 03, 90010280);
+INSERT INTO preco_veiculo (id_preco, id_veiculo, preco_dia, preco_semana, preco_mes)
+VALUES (12, 02, 300, 1890, 7200);
 
-INSERT INTO cliente_endereco (cod_endereco, nome_end, no_casa, complemento, bairro, cod_cidade, cep)
-VALUES (03, 'Rua Ferreira Viana', 76, 'ap 2', 'São José', 02, 92425140);
+INSERT INTO preco_veiculo (id_preco, id_veiculo, preco_dia, preco_semana, preco_mes)
+VALUES (13, 03, 400, 2520, 9600);
 
-INSERT INTO cliente_endereco (cod_endereco, nome_end, no_casa, complemento, bairro, cod_cidade, cep)
-VALUES (03, 'Rua Ricardo Samuel de Araujo', 123, 'casa 2', 'Jardim Figueiredo', 01, 08526030);
+INSERT INTO preco_veiculo (id_preco, id_veiculo, preco_dia, preco_semana, preco_mes)
+VALUES (14, 04, 250, 1575, 6000);
 
---Inserções na tabela id_cliente
-INSERT INTO id_cidade (cod_cidade, nome_cidade, uf)
-VALUES (01, 'Ferraz de Vasconcelos', 'SP');
+INSERT INTO preco_veiculo (id_preco, id_veiculo, preco_dia, preco_semana, preco_mes)
+VALUES (15, 05, 200, 1260, 4800);
 
-INSERT INTO id_cidade (cod_cidade, nome_cidade, uf)
-VALUES (02, 'Canoas', 'RS');
 
-INSERT INTO id_cidade (cod_cidade, nome_cidade, uf)
-VALUES (03, 'Porto Alegre', 'RS');
+--inserir infos sobre os veículos
+INSERT INTO veiculos (id_veiculo, placa, veiculo_nome, modelo, ano_veiculo, fabricante, no_passageiros, litros_portamalas, id_preco)
+VALUES (01, 'TWI1CE12', 'Cronos', 'Precision 1.3', 2023, 'Fiat', 5, 252, 11);
 
-INSERT INTO id_cidade (cod_cidade, nome_cidade, uf)
-VALUES (04, 'Niterói', 'RJ');
+INSERT INTO veiculos (id_veiculo, placa, veiculo_nome, modelo, ano_veiculo, fabricante, no_passageiros, litros_portamalas, id_preco)
+VALUES (02, 'ONC3E13', 'Spin', 'LTZ', 2023, 'Chevrolet', 7, 710, 12);
 
-INSERT INTO id_cidade (cod_cidade, nome_cidade, uf)
-VALUES (04, 'Salvador', 'BA');
+INSERT INTO veiculos (id_veiculo, placa, veiculo_nome, modelo, ano_veiculo, fabricante, no_passageiros, litros_portamalas, id_preco)
+VALUES (03, 'TZU2Y01', 'Jetta', 'GTI', 2021, 'Volkswagen', 5, 510 13);
+
+INSERT INTO veiculos (id_veiculo, placa, veiculo_nome, modelo, ano_veiculo, fabricante, no_passageiros, litros_portamalas, id_preco)
+VALUES (04, 'MIN4T02', 'Peugeot', '208', 2024, 'Citroen', 5, 285, 14);
+
+INSERT INTO veiculos (id_veiculo, placa, veiculo_nome, modelo, ano_veiculo, fabricante, no_passageiros, litros_portamalas, id_preco)
+VALUES (05, 'JIH1Y00', 'HB20', 'Comfort Plus', 2024, 'Hyundai', 5, 300, 15);
+
+
+--inserir informações sobre alugueis realizados
+INSERT INTO aluga (id_alug, cpf, id_veiculo, valor_alug, inicio_alug, fim_alug)
+VALUES (1, 3692581473, 04, 2250, '2023-09-03', '2023-09-13');
+
+INSERT INTO aluga (id_alug, cpf, id_veiculo, valor_alug, inicio_alug, fim_alug)
+VALUES (2, 12345678912, 03, 2520, '2023-08-18', '2023-08-25');
+
+INSERT INTO aluga (id_alug, cpf, id_veiculo, valor_alug, inicio_alug, fim_alug)
+VALUES (3, 78945612378, 02, 1200, '2023-09-01', '2023-09-04');
+
+INSERT INTO aluga (id_alug, cpf, id_veiculo, valor_alug, inicio_alug, fim_alug)
+VALUES (4, 15975385214, 01, 500, '2023-09-10', '2023-09-12');
+
+
+--inserir mnanutenções realizadas na tabela de manutenção (os preços são bem aproximados, porque eu não faço ideia de quanto custa, pois não dirijo nem tenho carro)
+INSERT INTO manutencao (id_manu, id_veiculo, valor_manu, inicio_manu, fim_manu, tipo_manu)
+VALUES (1, 02, 110, '2023-08-15', '2023-08-16', 'Troca de óleo');
+
+INSERT INTO manutencao (id_manu, id_veiculo, valor_manu, inicio_manu, fim_manu, tipo_manu)
+VALUES (2, 03, 400, '2023-09-01', '2023-09-02', 'Troca de fluido de transmissão');
+
+INSERT INTO manutencao (id_manu, id_veiculo, valor_manu, inicio_manu, fim_manu, tipo_manu)
+VALUES (3, 04, 400, '2023-09-28', '2023-09-29', 'Verificação dos freios e troca de pastilhas');
+
+INSERT INTO manutencao (id_manu, id_veiculo, valor_manu, inicio_manu, fim_manu, tipo_manu)
+VALUES (4, 05, 50, '2023-09-15', '2023-09-16', 'Troca de filtro de ar');
